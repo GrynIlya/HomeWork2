6c091de4fd9685dee37424ee585985d5f8e24d52
Gryn Ilya <grynilia@gmail.com>
2018-12-28 15:58:22 +0200

    new version

--- js/part1.js
+++ js/part1.js
@@ -3,9 +3,13 @@ let applesDiscount = getDiscount('apples');
 let orangesPrice = getPrice('oranges');
 let orangesProfit = getProfit('oranges');
 
-let applesCurrentPrice = applesPrice - applesPrice * applesDiscount / 100;
-let orangesCurrentPrice = orangesPrice + orangesPrice * orangesProfit / 100;
+if(applesPrice<0 || applesDiscount<0 || orangesPrice<0 || orangesProfit<0){
+	console.log('Введите корректную информацию');
+}else{
+	let applesCurrentPrice = applesPrice - applesPrice * applesDiscount / 100;
+	let orangesCurrentPrice = orangesPrice + orangesPrice * orangesProfit / 100;
 
-console.log(`
-Current apples price ${(Math.round(applesCurrentPrice * 100) / 100)}.
-Current oranges price ${(Math.round(orangesCurrentPrice * 100) / 100)}.`);
+	console.log(`
+	Current apples price ${(Math.round(applesCurrentPrice * 100) / 100)}.
+	Current oranges price ${(Math.round(orangesCurrentPrice * 100) / 100)}.`);
+}
