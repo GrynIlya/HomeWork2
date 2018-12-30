5f75e32ecf9306faa58ba3f7fe3b1e7a235ff6c7
Gryn Ilya <grynilia@gmail.com>
2018-12-30 14:15:25 +0200

    new version hw 4

--- js/part1.js
+++ js/part1.js
@@ -1,11 +1,47 @@
+// новый ценник на яблоки
+
 let applesPrice = getPrice('apples');
 let applesDiscount = getDiscount('apples');
+let totalPrice;
+
+console.log(!isNaN(applesDiscount));
+
+if(applesDiscount) {
+    if(isNaN(applesDiscount)) {
+        console.log('Ошибка!Вы ввели не число');
+    }else if(applesDiscount > 0 && applesDiscount < 100){
+        console.log('Вы ввели число');
+        totalPrice = applesPrice - applesPrice * (applesDiscount/100);
+        console.log(`Цена на яблоки:
+        ${totalPrice.toFixed(2)} грн`);
+    }else {
+        console.log('Скидка должная быть от 0 до 99');
+    }
+}else {
+    console.log(`Цена на яблоки:
+    ${applesPrice.toFixed(2)} грн`)
+}
+
+//новый ценник на апельсины
+
 let orangesPrice = getPrice('oranges');
 let orangesProfit = getProfit('oranges');
+let totalPrice;
 
-let applesCurrentPrice = applesPrice - applesPrice * applesDiscount / 100;
-let orangesCurrentPrice = orangesPrice + orangesPrice * orangesProfit / 100;
+console.log(!isNaN(orangesPrice));
 
-console.log(`
-Current apples price ${(Math.round(applesCurrentPrice * 100) / 100)}.
-Current oranges price ${(Math.round(orangesCurrentPrice * 100) / 100)}.`);
+if(orangesProfit) {
+    if (isNaN(orangesPrice)) {
+        console.log('Ошибка!Вы ввели не число');
+    } else if (orangesPrice > 0) {
+        console.log('Вы ввели положительное число');
+        totalPrice = orangesPrice + orangesPrice * (orangesProfit / 100);
+        console.log(`Цена на апельсины:
+        ${totalPrice.toFixed(2)} грн`);
+    } else {
+        console.log('Цена должна быть положительным числом');
+    }
+}else {
+    console.log(`Цена на апельсины:
+    ${orangesPrice.toFixed(2)} грн`)
+}
